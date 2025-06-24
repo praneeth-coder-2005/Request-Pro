3c81f4dc6ffa99644658ddd2eff92d33dabfdbeb
praneeth-coder-2005 <savediskofc@gmail.com>
2025-06-24 13:24:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-MOVIE_CHANNEL_ID = int(os.getenv("MOVIE_CHANNEL_ID")) # Your main movie channel
+MOVIE_CHANNEL_ID = (os.getenv("MOVIE_CHANNEL_ID")) # Your main movie channel
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID")) # Your personal chat ID
 
 # TMDB API Key - Get this from https://www.themoviedb.org/settings/api
