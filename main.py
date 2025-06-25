83a609322747d64f2f6ecaab23f9442667210b6f
praneeth-coder-2005 <savediskofc@gmail.com>
2025-06-25 13:46:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from pyrogram import Client, filters
-from handlers.start import start_handler
-from config import BOT_TOKEN, API_ID, API_HASH
+from pyrogram.types import Message
 
 app = Client(
     "movie_request_bot",
-    bot_token=BOT_TOKEN,
-    api_id=API_ID,
-    api_hash=API_HASH
+    bot_token="7549352926:AAGiDLjgMWBIH4VyuBCRHUfUkzCtx6bjlGg",
+    api_id=22250562,
+    api_hash="07754d3bdc27193318ae5f6e6c8016af"
 )
 
-# Register handlers
-app.add_handler(start_handler)
+@app.on_message(filters.private)
+async def echo(client, message: Message):
+    print("Message received!")
+    await message.reply("✅ Bot is working!")
 
-# Start the bot
 if __name__ == "__main__":
     print("Bot is running...")
     app.run()
