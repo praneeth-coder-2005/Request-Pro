8f662ba27bf08ea2f7acf6d151cd18c970557293
praneeth-coder-2005 <savediskofc@gmail.com>
2025-06-25 10:25:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_HASH = os.environ.get("API_HASH", "07754d3bdc27193318ae5f6e6c8016af")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7549352926:AAGiDLjgMWBIH4VyuBCRHUfUkzCtx6bjlGg")
 
 ADMIN_CHAT_ID = int(os.environ.get("ADMIN_CHAT_ID", "1894915577")) # Your numerical user ID
-MOVIE_CHANNEL_ID = int(os.environ.get("MOVIE_CHANNEL_ID", "@dumprjddisb")) # IMPORTANT: Numeric ID like -100123456789
+MOVIE_CHANNEL_ID = int(os.environ.get("MOVIE_CHANNEL_ID", "-1002444091857")) # IMPORTANT: Numeric ID like -100123456789
 
 # TMDB API Key
 TMDB_API_KEY = os.environ.get("TMDB_API_KEY", "bb5f40c5be4b24660cbdc20c2409835e")
