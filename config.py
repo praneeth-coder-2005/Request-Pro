8b50919da15d9a02b1dfbf5733a421998175a967
praneeth-coder-2005 <savediskofc@gmail.com>
2025-06-25 18:12:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-BOT_TOKEN = "7549352926:AAFE3CsRudvv1jk0KazBLoVlXZgwpvjGuco"
 API_ID = 22250562
 API_HASH = "07754d3bdc27193318ae5f6e6c8016af"
-
-TMDB_API_KEY = "bb5f40c5be4b24660cbdc20c2409835e"  # Public TMDb key
+BOT_TOKEN = "7549352926:AAFE3CsRudvv1jk0KazBLoVlXZgwpvjGuco"
+MONGODB_URI = "mongodb+srv://claw-earning:5213680099@cluster0.je3b1k7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
