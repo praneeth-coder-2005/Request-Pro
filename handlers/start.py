cc5c123be3b1242bdd2e20163c75d67d16ad5abf
praneeth-coder-2005 <savediskofc@gmail.com>
2025-06-25 17:46:15 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,23 +1,13 @@
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-async def handle_start(client, message: Message):
+async def send_welcome(client, message: Message):
     keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton("📥 Request Movie", callback_data="request_movie")],
-        [InlineKeyboardButton("📃 How It Works", callback_data="how_it_works")],
-        [InlineKeyboardButton("📢 Updates Channel", url="https://t.me/ClawMoviez")],
+        [InlineKeyboardButton("🎬 Request a Movie", callback_data="start_request")],
+        [InlineKeyboardButton("📢 Updates", url="https://t.me/ClawMoviez")]
     ])
 
-    welcome_text = f"""
-👋 **Welcome {message.from_user.first_name}!**
-
-🎬 I'm your Movie Request Bot!
-
-✅ Features:
-• Request any movie
-• Track your requests
-• Get fast admin replies
-
-Stay tuned and enjoy!
-    """
-
-    await message.reply(welcome_text, reply_markup=keyboard)
+    await message.reply(
+        "**👋 Welcome to Claw Movie Request Bot!**\n\n"
+        "Request any movie and we’ll try to find it for you.",
+        reply_markup=keyboard
+    )
