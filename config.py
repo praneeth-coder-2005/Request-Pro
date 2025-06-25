d1d2dac81a85b7d09a4946a5a062a46922255768
praneeth-coder-2005 <savediskofc@gmail.com>
2025-06-25 14:15:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 BOT_TOKEN = "7549352926:AAFE3CsRudvv1jk0KazBLoVlXZgwpvjGuco"
 API_ID = 22250562
 API_HASH = "07754d3bdc27193318ae5f6e6c8016af"
+MOVIE_CHANNEL = "dumprjddisb"  # ✅ Replace with your channel username (no @)
